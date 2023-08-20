f5259e92f55aac7edfe41f1875a75ed1a433c54b
HWANG INJAE <sub.ij002@gmail.com>
2023-08-20 20:57:26 +0900

    secure info

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+
 const {user} = require("./models/User");
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
@@ -10,14 +13,14 @@ app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://injae:kCvPefh2aDP17ZaZ@cluster0.nyzmmqy.mongodb.net/?retryWrites=true&w=majority').then(()=>console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI).then(()=>console.log('MongoDB Connected...'))
 .catch(err=>console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!')) //get 라우터
 
 app.post('/register', (req, res) => {
     const user = new User(req.body)
-    
+
     user.save((err, userInfo) =>{
       if (err) return res.json({success:false, err})
       return res.status(200).json({
